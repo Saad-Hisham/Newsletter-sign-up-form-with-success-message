82872fdaef25c9908e5df14328235d125574568f
Saad Hesham <132556058+Saad-Hisham@users.noreply.github.com>
2023-09-17 08:58:30 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+AOS.init();
+var emailInput = document.querySelector('input[name="email"]');
+
+document.getElementById('subscribe-btn').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent form submission
+
+  
+    var emailInput = document.querySelector('input[name="email"]');
+    var email = emailInput.value.trim();
+
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (emailRegex.test(email)) {
+      document.querySelector(".parent-container-success").style.display="flex"
+      document.querySelector(".parent-container").style.display="none"
+      // You can perform additional actions here, such as sending the form data to a server
+    } else {
+      document.querySelector("form").classList.add("err")
+    }
+  });
+  emailInput.onkeydown = function(){
+    document.querySelector("form").classList.remove("err")
+
+  }
+  document.querySelector(".parent-container-success button").onclick = function(){
+    document.querySelector(".parent-container-success").style.display="none"
+    document.querySelector(".parent-container").style.display="flex"
+  }
