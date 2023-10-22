08be035ff7fe851b30074188b8abbae4008d29b1
Saad Hesham <132556058+Saad-Hisham@users.noreply.github.com>
2023-10-22 22:05:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-AOS.init();
-var emailInput = document.querySelector('input[name="email"]');
-var span = document.querySelector('#em')
+// AOS.init();
+// var emailInput = document.querySelector('input[name="email"]');
+// var span = document.querySelector('#em')
 
-document.getElementById('subscribe-btn').addEventListener('click', function(event) {
-    event.preventDefault(); // Prevent form submission
+// document.getElementById('subscribe-btn').addEventListener('click', function(event) {
+//     event.preventDefault(); // Prevent form submission
 
   
-    var emailInput = document.querySelector('input[name="email"]');
-    var email = emailInput.value.trim();
+//     var emailInput = document.querySelector('input[name="email"]');
+//     var email = emailInput.value.trim();
 
-    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+//     var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-    if (emailRegex.test(email)) {
-      document.querySelector(".parent-container-success").style.display="flex"
-        span.textContent = emailInput.value;
-      document.querySelector(".parent-container").style.display="none"
-      // You can perform additional actions here, such as sending the form data to a server
-    } else {
-      document.querySelector("form").classList.add("err")
-    }
-  });
-  emailInput.onkeydown = function(){
-    document.querySelector("form").classList.remove("err")
+//     if (emailRegex.test(email)) {
+//       document.querySelector(".parent-container-success").style.display="flex"
+//         span.textContent = emailInput.value;
+//       document.querySelector(".parent-container").style.display="none"
+//       // You can perform additional actions here, such as sending the form data to a server
+//     } else {
+//       document.querySelector("form").classList.add("err")
+//     }
+//   });
+//   emailInput.onkeydown = function(){
+//     document.querySelector("form").classList.remove("err")
 
-  }
-  document.querySelector(".parent-container-success button").onclick = function(){
-    document.querySelector(".parent-container-success").style.display="none"
-    document.querySelector(".parent-container").style.display="flex"
-  }
+//   }
+//   document.querySelector(".parent-container-success button").onclick = function(){
+//     document.querySelector(".parent-container-success").style.display="none"
+//     document.querySelector(".parent-container").style.display="flex"
+//   }
